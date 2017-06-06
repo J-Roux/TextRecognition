68e09466c99459cb01f1003f76533ce3f53b88bf
Павел Арабей <aps7@tut.by>
2017-06-06 15:59:48 +0300

    first workable

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from scipy import ndimage
 import cv2
 
 a4_y_size_mm = 297
+a4_x_size_mm = 210
 
 plt.interactive(False)
 img = imread('page-33.png')
 a4_y_size_px = img.shape[0]
+a4_x_size_px = img.shape[1]
 gray_image = cvtColor(img, COLOR_BGR2GRAY)
 
-gray_image = ndimage.minimum_filter(gray_image, size=7)
-gray_image = Canny(gray_image,50,150,apertureSize = 5)
+gray_image = ndimage.minimum_filter(gray_image, size=12)
+gray_image = Canny(gray_image,50,150,apertureSize = 3)
 
 #plt.imshow(gray_image, cmap='gray')
 #plt.show()
 
 lines = HoughLines(gray_image,1,np.pi/180,200)
-for line in lines[:1]:
+for line in lines[-1:]:
     rho,theta = line[0]
     a = np.cos(theta)
     b = np.sin(theta)
@@ -29,6 +31,7 @@ for line in lines[:1]:
     y2 = int(y0 - 1000*(a))
     x2 = int(x0 - 1000 * (-b))
     print float(y1) / a4_y_size_px * a4_y_size_mm
+    print float(x1) / a4_x_size_px * a4_x_size_mm
     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 
